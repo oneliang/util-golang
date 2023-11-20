9de94d80cf7171ba0030e2f1c106ab7263608637
oneliang <oneliang@oneliang.com>
2023-11-20 10:26:54 +0800

    optimize base module

--- base/base.go
+++ base/base.go
@@ -9,3 +9,11 @@ const (
 func PrintName() {
 	fmt.Println("base/base.go")
 }
+
+type Also[T interface{}] interface {
+	Also(func(*T)) *T
+}
+
+type Let[T interface{}, R interface{}] interface {
+	Let(func(*T) *R) *R
+}
--- test/base_test.go
+++ test/base_test.go
@@ -9,4 +9,25 @@ import (
 func TestBase(t *testing.T) {
 	fmt.Print("test")
 	base.PrintName()
+	a := &A{}
+	a.Also(func(it *A) {
+		it.Name = ""
+	})
+	a.Let(func(it *A) *A {
+		it.Name = ""
+		return it
+	})
+}
+
+type A struct {
+	Name string
+}
+
+func (a *A) Also(block func(it *A)) *A {
+	block(a)
+	return a
+}
+
+func (a *A) Let(block func(it *A) *A) *A {
+	return block(a)
 }
