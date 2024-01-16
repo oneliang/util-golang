6100543b614daeb599307a27c3c6313799614f69
oneliang <oneliang@oneliang.com>
2024-01-16 09:54:09 +0800

    optimize some code

--- constants/constants.go
+++ constants/constants.go
@@ -1,6 +1,162 @@
 package constants
 
 const (
-	STRING_BLANK = ""
-	SYMBOL_AND   = "&"
+	STRING_BLANK    = ""
+	SYMBOL_DOT      = "."
+	SYMBOL_DOT_CHAR = '.'
+
+	/**
+	 * comma ","
+	 */
+	SYMBOL_COMMA = ","
+
+	/**
+	 * colon ":"
+	 */
+	SYMBOL_COLON = ":"
+
+	/**
+	 * semicolon ";"
+	 */
+	SYMBOL_SEMICOLON = ";"
+
+	/**
+	 * equal "="
+	 */
+	SYMBOL_EQUAL = "="
+
+	/**
+	 * and "&"
+	 */
+	SYMBOL_AND = "&"
+
+	/**
+	 * question mark "?"
+	 */
+	SYMBOL_QUESTION_MARK = "?"
+
+	/**
+	 * wildcard "*"
+	 */
+	SYMBOL_WILDCARD = "*"
+
+	/**
+	 * underline "_"
+	 */
+	SYMBOL_UNDERLINE = "_"
+
+	/**
+	 * at "@"
+	 */
+	SYMBOL_AT = "@"
+
+	/**
+	 * plus "+"
+	 */
+	SYMBOL_PLUS = "+"
+
+	/**
+	 * minus "-"
+	 */
+	SYMBOL_MINUS = "-"
+
+	/**
+	 * logic and "&&"
+	 */
+	SYMBOL_LOGIC_AND = "&&"
+
+	/**
+	 * logic or "||"
+	 */
+	SYMBOL_LOGIC_OR = "||"
+
+	/**
+	 * brackets begin "("
+	 */
+	SYMBOL_BRACKET_LEFT = "("
+
+	/**
+	 * brackets end ")"
+	 */
+	SYMBOL_BRACKET_RIGHT = ")"
+
+	/**
+	 * middle bracket left "["
+	 */
+	SYMBOL_MIDDLE_BRACKET_LEFT = "["
+
+	/**
+	 * middle bracket right "]"
+	 */
+	SYMBOL_MIDDLE_BRACKET_RIGHT = "]"
+
+	/**
+	 * big bracket "{"
+	 */
+	SYMBOL_BIG_BRACKET_LEFT = "{"
+
+	/**
+	 * big bracket "}"
+	 */
+	SYMBOL_BIG_BRACKET_RIGHT = "}"
+
+	/**
+	 * slash "/"
+	 */
+	SYMBOL_SLASH_LEFT = "/"
+
+	/**
+	 * slash "\"
+	 */
+	SYMBOL_SLASH_RIGHT = "\\"
+
+	/**
+	 * xor or regex begin "^"
+	 */
+	SYMBOL_XOR = "^"
+
+	/**
+	 * dollar or regex end "$"
+	 */
+	SYMBOL_DOLLAR = "$"
+
+	/**
+	 * single quote "'"
+	 */
+	SYMBOL_SINGLE_QUOTE = "'"
+
+	/**
+	 * double quote "\""
+	 */
+	SYMBOL_DOUBLE_QUOTE = "\""
+
+	/**
+	 * less than "<"
+	 */
+	SYMBOL_LESS_THAN = "<"
+
+	/**
+	 * greater than ">"
+	 */
+	SYMBOL_GREATER_THAN = ">"
+
+	/**
+	 * tilde "~"
+	 */
+	SYMBOL_TILDE = "~"
+
+	/**
+	 * accent "`"
+	 */
+	SYMBOL_ACCENT = "`"
+
+	/**
+	 * percent "%"
+	 */
+	SYMBOL_PERCENT = "%"
+
+	/**
+	 * pound key "#"
+	 */
+	SYMBOL_POUND = "#"
 )
