f1a494a49063821fde4f892899e42e7da2ae7ff3
oneliang <oneliang@oneliang.com>
2024-01-16 10:05:56 +0800

    optimize some constants tips

--- constants/constants.go
+++ constants/constants.go
@@ -1,162 +1,105 @@
 package constants
 
 const (
-	STRING_BLANK    = ""
-	SYMBOL_DOT      = "."
+	// STRING_BLANK  blank string
+	STRING_BLANK = ""
+
+	// SYMBOL_DOT symbol dot
+	SYMBOL_DOT = "."
+
+	// SYMBOL_DOT_CHAR
 	SYMBOL_DOT_CHAR = '.'
 
-	/**
-	 * comma ","
-	 */
+	// SYMBOL_COMMA comma ","
 	SYMBOL_COMMA = ","
 
-	/**
-	 * colon ":"
-	 */
+	// SYMBOL_COLON colon ":"
 	SYMBOL_COLON = ":"
 
-	/**
-	 * semicolon ";"
-	 */
+	// SYMBOL_SEMICOLON semicolon ";"
 	SYMBOL_SEMICOLON = ";"
 
-	/**
-	 * equal "="
-	 */
+	// SYMBOL_EQUAL equal "="
 	SYMBOL_EQUAL = "="
 
-	/**
-	 * and "&"
-	 */
+	// SYMBOL_AND and "&"
 	SYMBOL_AND = "&"
 
-	/**
-	 * question mark "?"
-	 */
+	// SYMBOL_QUESTION_MARK question mark "?"
 	SYMBOL_QUESTION_MARK = "?"
 
-	/**
-	 * wildcard "*"
-	 */
+	// SYMBOL_WILDCARD wildcard "*"
 	SYMBOL_WILDCARD = "*"
 
-	/**
-	 * underline "_"
-	 */
+	// SYMBOL_UNDERLINE underline "_"
 	SYMBOL_UNDERLINE = "_"
 
-	/**
-	 * at "@"
-	 */
+	// SYMBOL_AT at "@"
 	SYMBOL_AT = "@"
 
-	/**
-	 * plus "+"
-	 */
+	// SYMBOL_PLUS plus "+"
 	SYMBOL_PLUS = "+"
 
-	/**
-	 * minus "-"
-	 */
+	// SYMBOL_MINUS minus "-"
 	SYMBOL_MINUS = "-"
 
-	/**
-	 * logic and "&&"
-	 */
+	// SYMBOL_LOGIC_AND logic and "&&"
 	SYMBOL_LOGIC_AND = "&&"
 
-	/**
-	 * logic or "||"
-	 */
+	// SYMBOL_LOGIC_OR logic or "||"
 	SYMBOL_LOGIC_OR = "||"
 
-	/**
-	 * brackets begin "("
-	 */
+	// SYMBOL_BRACKET_LEFT brackets begin "("
 	SYMBOL_BRACKET_LEFT = "("
 
-	/**
-	 * brackets end ")"
-	 */
+	// SYMBOL_BRACKET_RIGHT brackets end ")"
 	SYMBOL_BRACKET_RIGHT = ")"
 
-	/**
-	 * middle bracket left "["
-	 */
+	// SYMBOL_MIDDLE_BRACKET_LEFT middle bracket left "["
 	SYMBOL_MIDDLE_BRACKET_LEFT = "["
 
-	/**
-	 * middle bracket right "]"
-	 */
+	// SYMBOL_MIDDLE_BRACKET_RIGHT middle bracket right "]"
 	SYMBOL_MIDDLE_BRACKET_RIGHT = "]"
 
-	/**
-	 * big bracket "{"
-	 */
+	// SYMBOL_BIG_BRACKET_LEFT big bracket "{"
 	SYMBOL_BIG_BRACKET_LEFT = "{"
 
-	/**
-	 * big bracket "}"
-	 */
+	// SYMBOL_BIG_BRACKET_RIGHT big bracket "}"
 	SYMBOL_BIG_BRACKET_RIGHT = "}"
 
-	/**
-	 * slash "/"
-	 */
+	// SYMBOL_SLASH_LEFT slash "/"
 	SYMBOL_SLASH_LEFT = "/"
 
-	/**
-	 * slash "\"
-	 */
+	// SYMBOL_SLASH_RIGHT slash "\"
 	SYMBOL_SLASH_RIGHT = "\\"
 
-	/**
-	 * xor or regex begin "^"
-	 */
+	// SYMBOL_XOR xor or regex begin "^"
 	SYMBOL_XOR = "^"
 
-	/**
-	 * dollar or regex end "$"
-	 */
+	// SYMBOL_DOLLAR dollar or regex end "$"
 	SYMBOL_DOLLAR = "$"
 
-	/**
-	 * single quote "'"
-	 */
+	// SYMBOL_SINGLE_QUOTE single quote "'"
 	SYMBOL_SINGLE_QUOTE = "'"
 
-	/**
-	 * double quote "\""
-	 */
+	// SYMBOL_DOUBLE_QUOTE double quote "\""
 	SYMBOL_DOUBLE_QUOTE = "\""
 
-	/**
-	 * less than "<"
-	 */
+	// SYMBOL_LESS_THAN less than "<"
 	SYMBOL_LESS_THAN = "<"
 
-	/**
-	 * greater than ">"
-	 */
+	// SYMBOL_GREATER_THAN greater than ">"
 	SYMBOL_GREATER_THAN = ">"
 
-	/**
-	 * tilde "~"
-	 */
+	// SYMBOL_TILDE tilde "~"
 	SYMBOL_TILDE = "~"
 
-	/**
-	 * accent "`"
-	 */
+	// SYMBOL_ACCENT accent "`"
 	SYMBOL_ACCENT = "`"
 
-	/**
-	 * percent "%"
-	 */
+	// SYMBOL_PERCENT percent "%"
 	SYMBOL_PERCENT = "%"
 
-	/**
-	 * pound key "#"
-	 */
+	// SYMBOL_POUND pound key "#"
 	SYMBOL_POUND = "#"
 )
