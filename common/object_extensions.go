001af7bc2c7f1b0000fa85d01340a38ca19db953
oneliang <oneliang@oneliang.com>
2024-02-29 14:47:41 +0800

    optimize some code

--- common/object_extensions.go
+++ common/object_extensions.go
@@ -1,6 +1,9 @@
 package common
 
-import "reflect"
+import (
+	"github.com/oneliang/util-golang/constants"
+	"reflect"
+)
 
 func ObjectInList[V interface{}](object V, list []V) bool {
 	for _, item := range list {
@@ -9,4 +12,16 @@ func ObjectInList[V interface{}](object V, list []V) bool {
 		}
 	}
 	return false
+}
+
+func JoinToString[V interface{}](list []V, transform func(index int, item V) string, separator string) string {
+	var results = ""
+	length := len(list)
+	for index, item := range list {
+		results += transform(index, item)
+		if index < length-1 {
+			results += separator + constants.STRING_SPACE
+		}
+	}
+	return results
 }
